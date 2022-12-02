11d44fe5c55a157125b26883510620e82c8fba5b
Shizuka Tanigawa <chocokkuma@gmail.com>
2022-12-02 17:39:05 +0100

    first commit

---
+++ src/main/java/com/example/buildingarestfulwebservicewithspringbootactuator/BuildingAresTfulWebServiceWithSpringBootActuatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.buildingarestfulwebservicewithspringbootactuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildingAresTfulWebServiceWithSpringBootActuatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildingAresTfulWebServiceWithSpringBootActuatorApplication.class, args);
+	}
+
+}
